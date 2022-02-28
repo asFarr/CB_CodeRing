110e8aa6ae8fd53271aadf222f88aa7563f6e6eb
asfarr <rasthlin12@gmail.com>
2022-02-28 09:26:37 -0500

    Optimized memory management and call structure for author() and helpme() dialogs Added module docstring to ring.py, currently a duplicate of helpme()

--- ring.py
+++ ring.py
@@ -1,4 +1,23 @@
-# TODO: Module docstring
+"""
+The CerealBox toolkit: Code Ring (v1.0) - by leToads : 2/25/22
+
+A script-ready tool for recursive encoding/decoding of
+input data provided from stdin or file input.
+
+    Usage: python ring.py {-a, -h}, {-e|-d} (-i <inputfile> -o <outputfile>), -t/-b
+    Examples:
+    ring.py -h   -   Display the help dialog.
+    ring.py -a   -   Display the author tag.
+    ring.py -et   -   Process console text input from stdin to encode.
+    ring.py -eb   -   Process console binary input from stdin to encode.
+    ring.py -e -i <infile> -o <outfile> -t   -   Process text file input to encode.
+    ring.py -e -i <infile> -o <outfile> -b   -   Process binary file input to encode.
+    ring.py -dt   -   Process console text input from stdin to decode.
+    ring.py -db   -   Process console binary input from stdin to decode.
+    ring.py -d -i <infile> -o <outfile> -t   -   Process text file input to decode.
+    ring.py -d -i <infile> -o <outfile> -b   -   Process binary file input to decode.
+
+"""
 
 import getopt
 import sys
@@ -9,60 +28,92 @@ import encoder
 
 def author():
     """Print the author ASCII signature."""
-    print("")
-    print("                   88         888888888888                               88             d8\'              ")
-    print("                   88              88                                    88            d8\'               ")
-    print("                   88              88                                    88           \"\"")
-    print("                   88   ,adPPYba,  88   ,adPPYba,   ,adPPYYba,   ,adPPYb,88  ,adPPYba,")
-    print("                   88  a8P_____88  88  a8\"     \"8a  \"\"     `Y8  a8\"    `Y88  I8[    \"\"")
-    print("                   88  8PP\"\"\"\"\"\"\"  88  8b       d8  ,adPPPPP88  8b       88   `\"Y8ba,")
-    print("                   88  \"8b,   ,aa  88  \"8a,   ,a8\"  88,    ,88  \"8a,   ,d88  aa    ]8I")
-    print("                   88   `\"Ybbd8\"'  88   `\"YbbdP\"'   `\"8bbdP\"Y8   `\"8bbdP\"Y8  `\"YbbdP\"'")
-    print("")
-    print("")
-    print("")
-    print("  ,ad8888ba,                                                   88  88888888ba")
-    print(" d8\"'    `\"8b                                                  88  88      \"8b                           ")
-    print("d8'                                                            88  88      ,8P                           ")
-    print("88              ,adPPYba,  8b,dPPYba,   ,adPPYba,  ,adPPYYba,  88  88aaaaaa8P'   ,adPPYba,  8b,     ,d8  ")
-    print("88             a8P_____88  88P'   \"Y8  a8P_____88  \"\"     `Y8  88  88\"\"\"\"\"\"8b,  a8\"     \"8a  `Y8, ,8P'   ")
-    print("Y8,            8PP\"\"\"\"\"\"\"  88          8PP\"\"\"\"\"\"\"  ,adPPPPP88  88  88      `8b  8b       d8    )888(")
-    print(" Y8a.    .a8P  \"8b,   ,aa  88          \"8b,   ,aa  88,    ,88  88  88      a8P  \"8a,   ,a8\"  ,d8\" \"8b,")
-    print("  `\"Y8888Y\"'    `\"Ybbd8\"'  88           `\"Ybbd8\"'  `\"8bbdP\"Y8  88  88888888P\"    `\"YbbdP\"'  8P'     `Y8")
-    print("")
-    print("")
-    print("")
-    print("  ,ad8888ba,                         88                       88888888ba   88")
-    print(" d8\"'    `\"8b                        88                       88      \"8b  \"\"                            ")
-    print("d8'                                  88                       88      ,8P                                ")
-    print("88              ,adPPYba,    ,adPPYb,88   ,adPPYba,           88aaaaaa8P'  88  8b,dPPYba,    ,adPPYb,d8  ")
-    print("88             a8\"     \"8a  a8\"    `Y88  a8P_____88           88\"\"\"\"88'    88  88P'   `\"8a  a8\"    `Y88  ")
-    print("Y8,            8b       d8  8b       88  8PP\"\"\"\"\"\"\"           88    `8b    88  88       88  8b       88")
-    print(" Y8a.    .a8P  \"8a,   ,a8\"  \"8a,   ,d88  \"8b,   ,aa           88     `8b   88  88       88  \"8a,   ,d88")
-    print("  `\"Y8888Y\"'    `\"YbbdP\"'    `\"8bbdP\"Y8   `\"Ybbd8\"'           88      `8b  88  88       88   `\"YbbdP\"Y8")
-    print("                                                                                             aa,    ,88")
-    print("                                                                                              \"Y8bbdP\"   ")
+    authorart = ["",
+                 "                   88         888888888888              "
+                 "                 88             d8\'              ",
+                 "                   88              88                   "
+                 "                 88            d8\'               ",
+                 "                   88              88                   "
+                 "                 88           \"\"",
+                 "                   88   ,adPPYba,  88   ,adPPYba,   ,adP"
+                 "PYYba,   ,adPPYb,88  ,adPPYba,",
+                 "                   88  a8P_____88  88  a8\"     \"8a  \""
+                 "\"     `Y8  a8\"    `Y88  I8[    \"\"",
+                 "                   88  8PP\"\"\"\"\"\"\"  88  8b       d"
+                 "8  ,adPPPPP88  8b       88   `\"Y8ba,",
+                 "                   88  \"8b,   ,aa  88  \"8a,   ,a8\"  8"
+                 "8,    ,88  \"8a,   ,d88  aa    ]8I",
+                 "                   88   `\"Ybbd8\"'  88   `\"YbbdP\"'   "
+                 "`\"8bbdP\"Y8   `\"8bbdP\"Y8  `\"YbbdP\"'",
+                 "",
+                 "",
+                 "",
+                 "  ,ad8888ba,                                           "
+                 "        88  88888888ba",
+                 " d8\"'    `\"8b                                        "
+                 "          88  88      \"8b                           ",
+                 "d8'                                                    "
+                 "        88  88      ,8P                           ",
+                 "88              ,adPPYba,  8b,dPPYba,   ,adPPYba,  ,adP"
+                 "PYYba,  88  88aaaaaa8P'   ,adPPYba,  8b,     ,d8  ",
+                 "88             a8P_____88  88P'   \"Y8  a8P_____88  \""
+                 "\"     `Y8  88  88\"\"\"\"\"\"8b,  a8\"     \"8a  `Y8, ,8P'   ",
+                 "Y8,            8PP\"\"\"\"\"\"\"  88          8PP\"\"\""
+                 "\"\"\"\"  ,adPPPPP88  88  88      `8b  8b       d8    )888(",
+                 " Y8a.    .a8P  \"8b,   ,aa  88          \"8b,   ,aa  "
+                 "88,    ,88  88  88      a8P  \"8a,   ,a8\"  ,d8\" \"8b,",
+                 "  `\"Y8888Y\"'    `\"Ybbd8\"'  88           `\"Ybbd8\""
+                 "'  `\"8bbdP\"Y8  88  88888888P\"    `\"YbbdP\"'  8P'     `Y8",
+                 "",
+                 "",
+                 "",
+                 "  ,ad8888ba,                         88            "
+                 "           88888888ba   88",
+                 " d8\"'    `\"8b                        88          "
+                 "             88      \"8b  \"\"                            ",
+                 "d8'                                  88            "
+                 "           88      ,8P                                ",
+                 "88              ,adPPYba,    ,adPPYb,88   ,adPPYba,"
+                 "           88aaaaaa8P'  88  8b,dPPYba,    ,adPPYb,d8  ",
+                 "88             a8\"     \"8a  a8\"    `Y88  a8P____"
+                 "_88           88\"\"\"\"88'    88  88P'   `\"8a  a8\"    `Y88  ",
+                 "Y8,            8b       d8  8b       88  8PP\"\"\"\""
+                 "\"\"\"           88    `8b    88  88       88  8b       88",
+                 " Y8a.    .a8P  \"8a,   ,a8\"  \"8a,   ,d88  \"8b,  "
+                 " ,aa           88     `8b   88  88       88  \"8a,   ,d88",
+                 "  `\"Y8888Y\"'    `\"YbbdP\"'    `\"8bbdP\"Y8   `\""
+                 "Ybbd8\"'           88      `8b  88  88       88   `\"YbbdP\"Y8",
+                 "                                                   "
+                 "                                          aa,    ,88",
+                 "                                                   "
+                 "                                           \"Y8bbdP\"   "]
+
+    for i in authorart:
+        print(i)
 
 
-def helpme():  # TODO: Add flag descriptions
+def helpme():
     """Print the help and usage dialog."""
-    print("The CerealBox toolkit Code Ring (v1.0) - by leToads : 2/25/22\n")
-    print("A script-ready tool for recursive encoding/decoding of input "
-          "data provided from stdin or file input. \n")
-    print("Usage: python ring.py {-a, -h}, {-e|-d} (-i <inputfile> -o <outputfile>), -t/-b\n\n")
+    usage = ["The CerealBox toolkit Code Ring (v1.0) - by leToads : 2/25/22\n",
+             "A script-ready tool for recursive encoding/decoding of input "
+             "data provided from stdin or file input. \n",
+             "Usage: python ring.py {-a, -h}, {-e|-d} (-i <inputfile> -o <outputfile>), -t/-b\n",
+             "Examples: ",
+             "ring.py -h   -   Display the help dialog. ",
+             "ring.py -a   -   Display the author tag.  \n",
+             "ring.py -et   -   Process console text input from stdin to encode. ",
+             "ring.py -eb   -   Process console binary input from stdin to encode. ",
+             "ring.py -e -i <infile> -o <outfile> -t   -   Process text file input to encode. ",
+             "ring.py -e -i <infile> -o <outfile> -b   -   Process binary file input to encode. \n",
+             "ring.py -dt   -   Process console text input from stdin to decode. ",
+             "ring.py -db   -   Process console binary input from stdin to decode. ",
+             "ring.py -d -i <infile> -o <outfile> -t   -   Process text file input to decode. ",
+             "ring.py -d -i <infile> -o <outfile> -b   -   Process binary file input to decode. "]
 
-    print("Examples: ")
-    print("ring.py -h   -   Display the help dialog. ")
-    print("ring.py -a   -   Display the author tag.  \n")
-    print("ring.py -et   -   Process console text input from stdin to encode. ")
-    print("ring.py -eb   -   Process console binary input from stdin to encode. ")
-    print("ring.py -e -i <infile> -o <outfile> -t   -   Process text file input to encode. ")
-    print("ring.py -e -i <infile> -o <outfile> -b   -   Process binary file input to encode. \n")
-    print("ring.py -dt   -   Process console text input from stdin to decode. ")
-    print("ring.py -db   -   Process console binary input from stdin to decode. ")
-    print("ring.py -d -i <infile> -o <outfile> -t   -   Process text file input to decode. ")
-    print("ring.py -d -i <infile> -o <outfile> -b   -   Process binary file input to decode. ")
-    sys.exit(2)
+    for i in usage:
+        print(i)
+
+    sys.exit(0)
 
 
 def main(argv):
